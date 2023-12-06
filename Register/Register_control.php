ccd5e2b63e1bc5873833849ae4259dea1f9235b2
suresh <choudharysuresh0407@gmail.com>
2023-12-06 19:13:07 +0530

    Today Working

--- Register/Register_control.php
+++ Register/Register_control.php
@@ -1,32 +1,26 @@
 <?php
 require_once(dirname(__FILE__, 2) . '/bootstrap.php');
-
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Handle form submission to add a new user
 
-    // Trim input values
-    $username = trim($_POST['username']);
-    $email = trim($_POST['email']);
-    $password = trim($_POST['password']);
- 
+        $username = $_POST['username'];
+        $email = $_POST['email'];
+        $id = $_POST['id'];
+
     // Basic form validation
-    if (empty($username) || empty($email) || empty($password)) {
+    if (empty($username) || empty($email) || empty($id)) {
         // Set error message in session
         $sessionHandler->setError("All fields are required.");
-
         // Redirect back to the registration page
         header('Location:./userregister.php');
         exit;
     } 
-    
-    // Set session variables
-    $_SESSION['username'] = $username;
-    $sessionHandler->setAuthUser($username);
+   
+    if($Register->getAllUsers()){
+        $sessionHandler->setMessage('Successfully registered');
+        header("Location:./userlist.php");
 
-    // Set success message
-    $sessionHandler->setMessage('Successfully registered');
-
-    // Redirect to the user list page
-    header("Location:./userlist.php");
-    exit();
+        exit();
+    }    
 }
 ?>
